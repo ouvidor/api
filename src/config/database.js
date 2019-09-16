03fe1feb656a2f9bfed9f70a0ed8a82c7afab3fe
Pedro Pinho <rihor1000@gmail.com>
2019-09-16 16:56:48 -0300

    Arquivo de configuração do DB separado Utiliza o bootstrap.js para passar uma config diferente para tests Esse arquivo é usado no database/index.js

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+require('../bootstrap');
+/**
+ * configuração de comunicação com o banco de dados
+ */
+module.exports = {
+  dialect: process.env.DB_DIALECT || 'mysql',
+  host: process.env.DB_DOMAIN,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+  storage: './__tests__/database.sqlite',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
