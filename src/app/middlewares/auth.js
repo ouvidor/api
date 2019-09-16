a844cc0a6e56e138ad4f8a22abae501baaf1f410
Pedro Pinho <rihor1000@gmail.com>
2019-09-16 14:49:07 -0300

    Tentando simplificar middlewares Removendo middleware vazio Renomeando middlewares.auth.js para auth.js Refatorando auth.js Utilizando verificação assincrona

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  // pega o token do header
+  const authHeader = req.headers.authorization;
+
+  // checa se é null
+  if (!authHeader) {
+    return res.status(401).send({ error: 'token não informado' });
+  }
+  const [scheme, token] = authHeader.split(' ');
+
+  // checa se a Header é no formato Bearer
+  if (scheme !== 'Bearer') {
+    return res.status(401).send({ error: 'Token em formato incorreto' });
+  }
+
+  try {
+    // transforma o jwt.verify em um método await
+    // dessa forma a função vira assincrona, evitando gargalo
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    // permite o acesso ao id do usuário apartir daqui
+    req.user_id = decoded.id;
+
+    // segue para o próximo middleware
+    return next();
+  } catch (err) {
+    // caso o token não passe na validação
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
