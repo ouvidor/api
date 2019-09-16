56773002998cb906a9caaf7b49e39e7f0dc96498
Pedro Pinho <rihor1000@gmail.com>
2019-09-16 14:10:05 -0300

    refatorando src/index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
-/*
-    Essa classe é o entry point da aplicação, aqui é iniciado o servidor na instacia
-    de app, iniciamos o "listen" do server e passamos o resto das responsabilidades para
-    routes.index
-*/
-
 import express from 'express';
+import cors from 'cors';
 
 // instancia configurada do Sequelize, para conectar ao Database
 import db from './database/database';
+import routes from './routes';
 
-// função com todas as rotas e middlewares da aplicação
-import Routes from './app/routes';
+class App {
+  constructor() {
+    this.server = express();
 
-const app = express();
+    this.initDatabase();
+    this.config();
+    this.routes();
+  }
 
-// inicia a conexão com o banco
-db.authenticate()
-  .then(() => console.log('database connected...'))
-  .catch(err => console.log(`error: ${err}`));
+  // iniciando a conexão com o banco
+  initDatabase() {
+    // inicia a conexão com o banco
+    db.authenticate()
+      .then(() => console.log('database connected...'))
+      .catch(err => console.log(`error: ${err}`));
+  }
 
-// Passa a nossa instancia de app para o routes.index ter acesso também.
-Routes(app);
+  // middlewares de configuração
+  config() {
+    // permite intenficação de json nas rotas
+    this.server.use(express.json());
 
-const port = process.env.LISTEN_PORT;
-app.listen(port, () => {
-  console.log(`Listening at port ${port}...`);
-});
+    // CORS permite acesso de qualquer ip à API
+    this.server.use(cors());
+
+    // isso abaixo é nessecario para que ao receber um Json do post, consiga ler ele como objeto sem problemas
+    this.server.use(express.urlencoded({ extended: true }));
+  }
+
+  // conectando as rotas ao app
+  routes() {
+    this.server.use(routes);
+  }
+
+  // inicia o servidor
+  init() {
+    const port = process.env.LISTEN_PORT;
+    this.server.listen(port, () => {
+      console.log(`Listening at port ${port}...`);
+    });
+  }
+}
+
+// cria uma instancia do App e inicia o servidor
+new App().init();
