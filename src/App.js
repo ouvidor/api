af32ecdfcef2362fab47503357ea84390e203ace
Pedro Pinho <rihor1000@gmail.com>
2019-09-16 14:15:41 -0300

    Separando responsabilidades Classe App ficando em arquivo separado index.js fica responsável apenas por iniciar o servidor

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+
+// instancia configurada do Sequelize, para conectar ao Database
+import db from './database/database';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.initDatabase();
+    this.config();
+    this.routes();
+  }
+
+  // iniciando a conexão com o banco
+  initDatabase() {
+    // inicia a conexão com o banco
+    db.authenticate()
+      .then(() => console.log('database connected...'))
+      .catch(err => console.log(`error: ${err}`));
+  }
+
+  // middlewares de configuração
+  config() {
+    // permite intenficação de json nas rotas
+    this.server.use(express.json());
+
+    // CORS permite acesso de qualquer ip à API
+    this.server.use(cors());
+
+    // isso abaixo é nessecario para que ao receber um Json do post, consiga ler ele como objeto sem problemas
+    this.server.use(express.urlencoded({ extended: true }));
+  }
+
+  // conectando as rotas ao app
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// cria uma instancia do App e inicia o servidor
+export default new App().server;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,7 @@
-import express from 'express';
-import cors from 'cors';
+import App from './App';
 
-// instancia configurada do Sequelize, para conectar ao Database
-import db from './database/database';
-import routes from './routes';
+const port = process.env.LISTEN_PORT;
 
-class App {
-  constructor() {
-    this.server = express();
-
-    this.initDatabase();
-    this.config();
-    this.routes();
-  }
-
-  // iniciando a conexão com o banco
-  initDatabase() {
-    // inicia a conexão com o banco
-    db.authenticate()
-      .then(() => console.log('database connected...'))
-      .catch(err => console.log(`error: ${err}`));
-  }
-
-  // middlewares de configuração
-  config() {
-    // permite intenficação de json nas rotas
-    this.server.use(express.json());
-
-    // CORS permite acesso de qualquer ip à API
-    this.server.use(cors());
-
-    // isso abaixo é nessecario para que ao receber um Json do post, consiga ler ele como objeto sem problemas
-    this.server.use(express.urlencoded({ extended: true }));
-  }
-
-  // conectando as rotas ao app
-  routes() {
-    this.server.use(routes);
-  }
-
-  // inicia o servidor
-  init() {
-    const port = process.env.LISTEN_PORT;
-    this.server.listen(port, () => {
-      console.log(`Listening at port ${port}...`);
-    });
-  }
-}
-
-// cria uma instancia do App e inicia o servidor
-new App().init();
+App.listen(port, () => {
+  console.log(`Listening at port ${port}...`);
+});
