623cc8151e08dd562e24c0c97b06d490bb8e8b14
Pedro Pinho <rihor1000@gmail.com>
2019-09-16 16:53:30 -0300

    Mudando a forma como os models são agregados e como a conexão com o DB é feita Criado arquivo database/index.js para gerenciar a conexão com o DB e gerir os models Esse arquivo bota as associações dos models em pratica e inicia a conexão

---
+++ src/database/index.js
@@ -0,0 +1,31 @@
+import Sequelize from 'sequelize';
+
+// configuração de acesso ao banco
+import databaseConfig from '../config/database';
+
+// importações dos models
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+}
+
+export default new Database();
