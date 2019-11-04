8b4fae3a82888eef42cbc7b8525ebceec6b837ff
Pedro Pinho <rihor1000@gmail.com>
2019-11-04 23:53:34 +0000

    concertando erro

--- __tests__/integration/role.test.js
+++ __tests__/integration/role.test.js
@@ -5,7 +5,6 @@ import app from '../../src/App';
 import factory from '../factories';
 import truncate from '../util/truncate';
 import registerUser from '../util/registerUser';
-import signInUser from '../util/signInUser';
 
 describe('Role', () => {
   // entre todos os testes é feito o truncate da tabela
@@ -24,9 +23,34 @@ describe('Role', () => {
       .set('Authorization', `Bearer ${token}`)
       .send();
 
-    expect(response.body[0]).toEqual(expect.objectContaining({level: 1, title: 'master'}));
-    expect(response.body[1]).toEqual(expect.objectContaining({level: 2, title: 'admin'}));
-    expect(response.body[2]).toEqual(expect.objectContaining({level: 3, title: 'citizen'}));
+    expect(response.body[0]).toEqual(expect.objectContaining({ level: 1, title: 'master' }));
+    expect(response.body[1]).toEqual(expect.objectContaining({ level: 2, title: 'admin' }));
+    expect(response.body[2]).toEqual(expect.objectContaining({ level: 3, title: 'citizen' }));
     expect(response.status).toBe(200);
+  });
+
+  it("citizens shouldn't list all roles", async () => {
+    const citizen = {
+      first_name: 'user',
+      last_name: 'tester',
+      email: 'user@gmail.com',
+      password: '123456'
+    };
+
+    await request(app).post('/user').send(citizen);
+
+    const loginResponse = await request(app).post('/auth').send(citizen);
+    expect(loginResponse.status).toBe(200);
+
+    const { token } = loginResponse.body;
+
+    const response = await request(app)
+      .get('/role')
+      .set('Authorization', `Bearer ${token}`)
+      .send();
+
+    expect(response.status).toBe(401);
+    expect(response.body).toHaveProperty('error');
+    expect(response.body.error).toStrictEqual('Acesso exclusivo para Admins');
   });
 });
