d82c7318025c9d44ffc580714d01380f9881936c
Pedro Pinho <rihor1000@gmail.com>
2019-11-12 17:08:11 -0300

    Concertando test de service

--- __tests__/unit/searchManifestationService.test.js
+++ __tests__/unit/searchManifestationService.test.js
@@ -3,8 +3,7 @@ import truncate from '../util/truncate';
 import Manifestation from '../../src/app/models/Manifestation';
 import Type from '../../src/app/models/Type';
 import Category from '../../src/app/models/Category';
-import searchManifestationService from '../../src/app/services/ManifestationService';
-import Category from '../../src/app/models/Category';
+import searchManifestationService from '../../src/app/services/SearchManifestationService';
 import User from '../../src/app/models/User';
 
 describe('Search Manifestation Service', () => {
@@ -26,7 +25,7 @@ describe('Search Manifestation Service', () => {
       email: 'email@gmail.com',
       first_name: 'Nome',
       last_name: 'Sobrenome',
-      password: '123456'
+      password: '123456',
     });
 
     try {
@@ -34,7 +33,7 @@ describe('Search Manifestation Service', () => {
         title: 'rua',
         description: 'descrição',
         type_id: type.id,
-        user_id: user.id
+        user_id: user.id,
       });
 
       await manifestation.setCategories([category.id]);
@@ -42,18 +41,22 @@ describe('Search Manifestation Service', () => {
       console.log(error);
     }
 
-    const manifestations = await searchManifestationService.search('rua');
+    const manifestations = await searchManifestationService.run('rua');
     expect(manifestations).toBeDefined();
     expect(manifestations[0]).toBeDefined();
-    expect(manifestations[0]).toEqual(expect.objectContaining({
-      title: 'rua',
-      description: 'descrição',
-      latitude: null,
-      type_id: type.id
-    }));
-    expect(manifestations[0].categories[0]).toEqual(expect.objectContaining({
-      title: 'Saneamento',
-      id: category.id
-    }));
+    expect(manifestations[0]).toEqual(
+      expect.objectContaining({
+        title: 'rua',
+        description: 'descrição',
+        latitude: null,
+        type_id: type.id,
+      })
+    );
+    expect(manifestations[0].categories[0]).toEqual(
+      expect.objectContaining({
+        title: 'Saneamento',
+        id: category.id,
+      })
+    );
   });
 });
