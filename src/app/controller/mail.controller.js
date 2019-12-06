e46a78a1ee66d61a63077ed443ed0f4d43ec102f
Pedro Pinho <rihor1000@gmail.com>
2019-12-06 14:28:52 -0300

    Documentando nova rota e concertando retorno da rota

--- src/app/controller/mail.controller.js
+++ src/app/controller/mail.controller.js
@@ -15,10 +15,14 @@ class MailController {
       });
     } catch (err) {
       console.error(err);
-      return res.status(500).json(`Não pode enviar o email: ${title}`);
+      return res
+        .status(500)
+        .json({ error: `Não pode enviar o email: ${title}` });
     }
 
-    return res.status(200).json(`Email: ${title}. enviado com sucesso`);
+    return res
+      .status(200)
+      .json({ message: `Email: ${title}. enviado com sucesso` });
   }
 } // fim da classe
 
