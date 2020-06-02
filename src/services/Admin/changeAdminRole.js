b20177bbae4ca75759b1eb51317b3c61b0fa170b
Pedro Pinho <rihor1000@gmail.com>
2020-06-01 22:04:11 -0300

    regra de negócio na mudança de role no service de admin

--- src/services/Admin/changeAdminRole.js
+++ src/services/Admin/changeAdminRole.js
@@ -1,12 +1,19 @@
 import AppError from '../../errors/AppError';
 import User from '../../models/User';
-import { ADMIN, CITIZEN } from '../../data/roles';
+import { MASTER, ADMIN, CITIZEN } from '../../data/roles';
 
 const changeAdminRole = async ({ id, adminRole = false }) => {
   const user = await User.findByPk(id);
 
   if (!user) {
-    throw AppError('Usuário não existe.', 404);
+    throw new AppError('Usuário não existe.', 404);
+  }
+
+  if (user.role === MASTER) {
+    throw new AppError(
+      'Não é permitido mudar o status do administrador master',
+      403
+    );
   }
 
   return user.update({ role: adminRole ? ADMIN : CITIZEN });
