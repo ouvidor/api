0a3050c4b0472b9503aa0ae0167ebf8453ce3acb
Pedro Pinho <rihor1000@gmail.com>
2020-06-13 14:40:40 -0300

    Add regra de negócio, secretarias de outras cidades não podem ser vistas

--- src/services/Secretary/fetch.js
+++ src/services/Secretary/fetch.js
@@ -1,7 +1,19 @@
+import AppError from '../../errors/AppError';
+import Prefecture from '../../models/Prefecture';
 import Secretary from '../../models/Secretary';
 
-const fetchSecretariats = async () => {
+const fetchSecretariats = async ({ city }) => {
+  const prefecture = await Prefecture.findOne({ where: { name: city } });
+
+  if (!prefecture) {
+    throw new AppError(
+      'Essa cidade não existe, tente relogar no sistema.',
+      404
+    );
+  }
+
   const secretariats = await Secretary.findAll({
+    where: { prefectures_id: prefecture.id },
     attributes: { exclude: ['created_at', 'updated_at'] },
   });
 
