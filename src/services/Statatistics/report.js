bdb731d087c1834394060ad8a0dd213fda7ef969
Pedro Pinho <rihor1000@gmail.com>
2020-06-14 19:45:55 -0300

    Implementando queries finais

--- src/services/Statatistics/report.js
+++ src/services/Statatistics/report.js
@@ -1,33 +1,41 @@
-import { format } from 'date-fns';
-
 import database from '../../database';
 
-const generateReport = async ({ init, end, city }) => {
-  const formattedInitialDate = format(init, 'dd/MM/yyyy');
-  const formattedEndDate = format(end, 'dd/MM/yyyy');
-
+const generateReport = async ({ init, end, cityName }) => {
+  /**
+   * Autor da query: Lucas Sousa
+   */
   const result = await database.query(`
     SELECT
       t.secretariats_id AS id,
-      s.title, s.accountable,
+      s.title,
+      s.accountable,
       SUM(IF(t.status_id = 5, 1, 0)) AS encerradas,
       SUM(IF(t.status_id != 5, 1, 0)) AS semResposta
     FROM
-    (
+      (
       SELECT
-          m.secretariats_id, msh.status_id
-      FROM manifestations_status_history msh
-      LEFT JOIN manifestations m ON m.id = msh.manifestations_id
-      WHERE msh.id
-      IN (
-          SELECT
-              MAX(id)
-          FROM manifestations_status_history
-          GROUP BY manifestations_id
-      )
-    )t
-    LEFT JOIN secretariats s ON s.id = t.secretariats_id
-    GROUP BY t.secretariats_id;
+        m.secretariats_id, msh.status_id, m.ombudsmen_id
+      FROM
+        manifestations_status_history msh
+      LEFT JOIN manifestations m ON
+        m.id = msh.manifestations_id
+      WHERE
+        DATE(msh.created_at) BETWEEN '${init}' AND '${end}'
+        AND msh.id IN (
+        SELECT
+          MAX(id)
+        FROM
+          manifestations_status_history
+        GROUP BY
+          manifestations_id ) )t
+    LEFT JOIN secretariats s ON
+      s.id = t.secretariats_id
+    LEFT JOIN prefectures p ON
+      p.ombudsmen_id = t.ombudsmen_id
+    WHERE
+      p.name = '${cityName}'
+    GROUP BY
+      t.secretariats_id
   `);
 
   return result;
--- src/services/Statatistics/statistic.js
+++ src/services/Statatistics/statistic.js
@@ -1,26 +1,34 @@
 import database from '../../database';
 
-const generateStatistic = async ({ init, end }) => {
+const generateStatistic = async ({ init, end, cityName }) => {
   /**
    * Autor da query: Lucas Sousa
    */
   const statistic = await database.query(`
     SELECT
-      CONCAT(DATE_FORMAT(created_at, '%m'), '#', YEAR(created_at)) AS mesAno,
+      CONCAT(DATE_FORMAT(t.created_at, '%m'), '#', YEAR(t.created_at)) AS mesAno,
       SUM(IF(t.types_id = 1, 1, 0)) AS sugestão,
       SUM(IF(t.types_id = 2, 1, 0)) AS elogio,
       SUM(IF(t.types_id = 3, 1, 0)) AS solicitacao,
       SUM(IF(t.types_id = 4, 1, 0)) AS reclamacao,
       SUM(IF(t.types_id = 5, 1, 0)) AS denuncia
     FROM
-    (
+      (
       SELECT
         *
-      FROM manifestations m
-      WHERE DATE(m.created_at) BETWEEN '${init}' AND '${end}'
-    )t
-    GROUP BY CONCAT(DATE_FORMAT(t.created_at, '%m'), '#', YEAR(t.created_at))
-    ORDER BY MAX(YEAR(created_at)) DESC, MAX(MONTH(created_at)) DESC;
+      FROM
+        manifestations m
+      WHERE
+        DATE(m.created_at) BETWEEN '${init}' AND '${end}' )t
+    LEFT JOIN prefectures p ON
+      p.ombudsmen_id = t.ombudsmen_id
+    WHERE
+      p.name = '${cityName}'
+    GROUP BY
+      CONCAT(DATE_FORMAT(t.created_at, '%m'), '#', YEAR(t.created_at))
+    ORDER BY
+      MAX(YEAR(t.created_at)) DESC,
+      MAX(MONTH(t.created_at)) DESC
   `);
 
   return statistic;
